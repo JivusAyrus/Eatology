92103dc3dcd9523927042819d2241b96e70a35c9
Vineet Kalghatgi <vkalghat@gmail.com>
2020-03-02 23:21:49 +0530

    added dynamic loading from spoonacular api  in homepage

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import '../Css/Card.css'
 import $ from "jquery";
 export class Card extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            title : props.title,
+            img: props.img
+            //var element = document.create
+        }
+    }
+
     componentDidMount() {
         $(document).ready(function () {
             $('.heart').on('click', function () {
@@ -13,8 +24,8 @@ export class Card extends Component {
         return (
             <div>
                 <div class="custcar">
-                    <img src={require('./a.jpg')} class="card-img-top" alt="..." width="300" height="200"/>
-                    <p class="car-title inline">Maggie<a className="heart"><i class="fa fa-heart-o" aria-hidden="true"></i></a></p>
+                    <img src= {this.state.img} class="card-img-top" alt="..." width="300" height="200"/>
+                    <p class="car-title inline">{this.state.title}<a className="heart"><i class="fa fa-heart-o" aria-hidden="true"></i></a></p>
                 </div>
             </div>
         )
