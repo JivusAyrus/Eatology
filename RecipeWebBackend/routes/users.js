9d4e22a789341442fe7bcec5b18d4e4ff0bb0c94
Vineet Kalghatgi <vkalghat@gmail.com>
2020-03-21 19:43:38 +0530

    modified update user route

--- RecipeWebBackend/routes/users.js
+++ RecipeWebBackend/routes/users.js
@@ -48,14 +48,13 @@ router.route('/by-email/:email').get((req, res) => {
 })
 
 // localhost/users/add will add a user whose object is specified in the request body.
-router.route('/add').post(upload.single('userImage'), (req, res) => {
+router.route('/add').post(upload.single('profile_img'), (req, res) => {
     if (req.file != undefined) {
         const username = req.body.username
         const fullname = req.body.fullname
         const email = req.body.email
         const password = req.body.password
         const profile_img = req.file.buffer
-        console.log(req.file.buffer)
         const newUser = User({
             username,
             fullname,
@@ -87,35 +86,18 @@ router.route('/add').post(upload.single('userImage'), (req, res) => {
 })
 
 // localhost/users/update/:id will update the user whose id is specified
-// router.route('/update/:id').post(upload.single('userImage'), (req, res) => {
-//     console.log(req.file)
-//     var username
-//     User.findById(req.params.id)
-//         .then(users => {
-//             username = users.username
-//             if (req.file != undefined) {
-//                 User.findOneAndUpdate({ _id: req.params.id }, {
-//                     username: req.body.username,
-//                     fullname: req.body.fullname,
-//                     email: req.body.email,
-//                     profile_img: req.file.path
-//                 })
-//                     .then(() => res.json(`${username} has been successfully updated !`))
-//                     .catch(err => res.status(400).json('Error : ' + err));
-//             }
-//             else {
-//                 User.findOneAndUpdate({ _id: req.params.id }, {
-//                     username: req.body.username,
-//                     fullname: req.body.fullname,
-//                     email: req.body.email,
-//                 })
-//                     .then(() => res.json(`${username} has been successfully updated !`))
-//                     .catch(err => res.status(400).json('Error : ' + err));
-//             }
-//         })
-//         .catch(err => res.status(400).json('Error : ' + err));
+router.route('/update/:id').post(upload.single('profile_img'), (req, res) => {
+    var username
+    User.findById(req.params.id)
+        .then(users => {
+            username = users.username
+            User.findOneAndUpdate({ _id: req.params.id }, req.body)
+                .then(() => res.json(`${username} has been successfully updated !`))
+                .catch(err => res.status(400).json('Error : ' + err));
+        })
+        .catch(err => res.status(400).json('Error : ' + err));
 
-// })
+})
 
 router.route('/update/add-favourite/:id').post((req, res) => {
     var username
@@ -140,7 +122,7 @@ router.route('/update/remove-favourite/:id').post((req, res) => {
         .then(users => {
             username = users.username
             User.findOneAndUpdate({ _id: req.params.id }, {
-                $pull: { favourites: {$in :[ req.body.favourite] }}
+                $pull: { favourites: { $in: [req.body.favourite] } }
 
             })
                 .then(() => res.json(`${username} has been successfully updated !`))
