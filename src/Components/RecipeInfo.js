353394414de5d5117d1819cc8916f2757f8ff176
JivusAyrus <suvijsurya76@gmail.com>
2020-04-24 19:29:24 +0530

    Added various list of recipes to the home page

--- src/Components/RecipeInfo.js
+++ src/Components/RecipeInfo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import '../Css/RecipeInfo.css'
 export class RecipeInfo extends Component {
     render() {
         return (
             <div>
-                
+                <img src="" alt="" width="100%" height="350px"/>
+                <h3 class="rectitle"></h3>
             </div>
         )
     }
