79c9ae78b729edfa5b04de00f750afe57745d57b
Vineet Kalghatgi <vkalghat@gmail.com>
2020-04-30 22:16:50 +0530

    added a shopping list model schema

---
+++ RecipeWebBackend/models/shoppingList.model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ShoppingListSchema = new Schema({
+    title : {
+        required : true,
+        type : String,
+        trim : true
+    },
+
+    list_items : {
+        type : [String]
+    }
+})
+
+const ShoppingList = mongoose.model('ShoppingList', ShoppingListSchema);
+module.exports = {
+    model : ShoppingList,
+    schema : ShoppingListSchema
+}
